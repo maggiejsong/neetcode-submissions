32c09270d25418d4791883b2817ecdbb56061f0a
Maggie Song <148509151+maggiejsong@users.noreply.github.com>
2026-09-21 15:24:35 -0400

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        res = 0
+        for i in range(lens(nums)):
+            sum = 0
+            for j in range(i, len(nums)):
+                sum += nums[j]
+                if sum == k:
+                    res += 1
+        return res
+        
