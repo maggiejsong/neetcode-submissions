535f30fe9d64979eba7f1f3e0c814549f0842d43
Maggie Song <148509151+maggiejsong@users.noreply.github.com>
2026-09-25 01:47:46 -0400

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.py
@@ -0,0 +1,37 @@
+'''
+Input = "abba"
+Output = "abba"
+'''
+
+# since each palindrome has a center we can split thi sinto even and odd cases 
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        resIdx = 0 
+        resLen = 0
+
+        for i in range(len(s)):
+            # odd lens
+            left = i
+            right = i
+            while left >= 0 and right <= len(s) and s[left] == s[right]:
+                if (right - left + 1) > resLen:
+                    resIdx = left
+                    resLen = r - l + 1
+                
+                left -= 1
+                right += 1
+
+            # even lens
+            left = i
+            right = i + 1
+            while left >= 0 and right <= len(s) and s[left] == s[right]:
+                if (right - left + 1) > resLen:
+                    resIdx = left
+                    resLen = r - l + 1
+                
+                left -= 1
+                right += 1
+        
+        return s[resIdx : resIdx + resLen]
+        
+        
